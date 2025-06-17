f3e94fa808c26ba2a7bfd34cbfb5824c1da40de6
cletus04jact <cletusjact04@gmail.com>
2025-06-17 11:45:05 +0530

    Sring basic

---
+++ String_basics.java
@@ -0,0 +1,10 @@
+public class String_basics {
+    public static void main(String[] args) {
+        String name="Cletus";
+        String name2="Sylphia";
+
+        // Concatenation
+        String fullName = name + " " + name2;
+        System.out.println("Full Name: " + fullName);
+    }
+}
